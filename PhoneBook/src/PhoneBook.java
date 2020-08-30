f641612c350b9acba482bfb8bfda776511acfc83
nekto007 <nekto.korobko@gmail.com>
2020-08-30 21:13:49 +0300

    Создание телефонного справочника.

--- PhoneBook/src/PhoneBook.java
+++ PhoneBook/src/PhoneBook.java
@@ -1,30 +1,78 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class PhoneBook {
 
     public static void main(String[] args) {
-        //Добавить считывание ввода пользователя в цикле
+        String[][] book = new String[1][2];
+        String name = "";
+        String phoneNumber = "";
+        Scanner scanner = new Scanner(System.in);
+        boolean isCorrectName = false;
+        boolean isCorrectPhone = false;
+        while (!isCorrectName) {
+            System.out.print("Введите Фамилию, Имя, Отчество (через пробел): ");
+            name = scanner.nextLine();
+            isCorrectName = checkName(name);
+            if (!isCorrectName) {
+                System.out.println("Введите корректное имя!");
+            }
+        }
+        while (!isCorrectPhone) {
+            System.out.print("Введите номер телефона в формате X XXX XXX XX XX: ");
+            phoneNumber = scanner.nextLine();
+            isCorrectPhone = checkPhoneNumber(phoneNumber);
+            if (!isCorrectPhone) {
+                System.out.println("Введите корректный номер телефона!");
+            }
+        }
+        add(book, formatName(name), formatPhoneNumber(phoneNumber));
+        list(book);
     }
 
     public static boolean checkPhoneNumber(String phoneNumber) {
-        return true;
+        String cleanNumber = phoneNumber.trim().replaceAll("[^0-9]", "");
+        return cleanNumber.length() == 11;
     }
 
     public static boolean checkName(String name) {
-        return true;
+        String[] words = name.trim().split(" ");
+        return words.length == 3;
     }
 
+
     public static String formatName(String name) {
-        return "";
+        String[] words = name.trim().split(" ");
+        String formattedName = "";
+        for (int i = 0; i < words.length; i++) {
+            String str = words[i];
+            char firstChar = str.charAt(0);
+            if (!Character.isUpperCase(firstChar)) {
+                formattedName += Character.toUpperCase(firstChar) + words[i].substring(1) + " ";
+            } else {
+                formattedName = name;
+            }
+
+        }
+        return formattedName;
     }
 
     public static String formatPhoneNumber(String number) {
-        return "";
+        String cleanNumber = number.replaceAll("[^0-9]", "");
+        return "+7" + " " + cleanNumber.substring(1, 4) + " " +
+                cleanNumber.substring(4, 7) + " " + cleanNumber.substring(7, 9) + " " + cleanNumber.substring(9);
     }
 
     public static void add(String[][] book, String name, String number) {
-        //add logic
+        book[0][0] = name;
+        book[0][1] = number;
     }
 
     public static void list(String[][] book) {
-        //print phone book
+
+        for (int i = 0; i < book.length; i++) {
+            System.out.println(Arrays.deepToString(book));
+            System.out.printf("%s: %s", book[0][0], book[0][1]);
+        }
     }
 }
